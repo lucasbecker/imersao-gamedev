80b1c75b9f40a9ddd1279ee7206c17870122864e
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-06-23 12:11:32 -0300

    Update Dia 2

--- README.md
+++ README.md
@@ -25,8 +25,19 @@ DIA 5 - Adicionando complexidade
 Movimentação diferente dos inimigos e powerups.
 
 ## Aula 1: Animações básicas e trilha sonora
+Utilizando a biblioteca JavaScript para animações, a p5.js, para criar as animações iniciais do jogo, do personagem ao background. Assim como, adicionar a trilha sonora do jogo.
+
 Desafios:
 - [ ] Organizar a matriz no personagem.js, tirando os números fixos e substituindo-os por um cálculo.
 - [ ] Criar a história da bruxa Hipsta. Por que ela está andando nessa floresta?
 - [ ] Fazer seus próprios sprites, criando um jogo com visual bem diferente! Homenageie seu heroi preferido, seu youtuber mais amado :).
 - [ ] Deixar a função de imagem mais limpa e organizada!
+
+## Aula 2: Fluxo de vida, missão e falha
+Começando a dar vida ao endless runner desenvolvendo a matemática por trás dos pulos, da gravidade e da colisão.
+
+Desafios:
+- [ ] Fixe o pulo duplo em no máximo 2!
+- [ ] Faça um background com aparência 3D usando duas imagens diferentes, com velocidades diferentes. É o efeito paralaxe. Para isso, você precisa que o da frente tenha transparência
+- [ ] Coloque elementos diferentes em seu jogo: abelhas, borboletas, o que você achar que vai ficar legal!
+- [ ] Faça uma tela de game over mais elaborada, explicando o que aconteceu com a personagem e porque a pessoa perdeu.
--- sketch.js
+++ sketch.js
@@ -1,22 +1,87 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
+
 let cenario;
 let trilha;
+let somDoPulo;
 let personagem;
+let inimigo;
+
+const matrizPersonagem = [
+  [0,0],
+  [220,0],
+  [440,0],
+  [660,0],
+  [0,270],
+  [220,270],
+  [440,270],
+  [660,270],
+  [0,540],
+  [220,540],
+  [440,540],
+  [660,540],
+  [0,810],
+  [220,810],
+  [440,810],
+  [660,810],
+]
+
+const matrizInimigo = [
+  [0, 0],
+  [104, 0],
+  [208, 0],
+  [312, 0],
+  [0, 104],
+  [104, 104],
+  [208, 104],
+  [312, 104],
+  [0, 208],
+  [104, 208],
+  [208, 208],
+  [312, 208],
+  [0, 312],
+  [104, 312],
+  [208, 312],
+  [312, 312],
+  [0, 418],
+  [104, 418],
+  [208, 418],
+  [312, 418],
+  [0, 522],
+  [104, 522],
+  [208, 522],
+  [312, 522],
+  [0, 626],
+  [104, 626],
+  [208, 626],
+  [312, 626],
+]
 
 function preload(){
   imagemCenario = loadImage("img/cenario/floresta.png");
   imagemPersonagem = loadImage("img/personagem/correndo.png");
+  imagemInimigo = loadImage("img/inimigos/gotinha.png");
   trilha = loadSound("sound/trilha_jogo.mp3");
+  somDoPulo = loadSound("sound/somPulo.mp3");
   
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 1);
-  personagem = new Personagem(imagemPersonagem);
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width-52, 52, 52, 104, 104);
+  
   frameRate(40);
   trilha.loop();
+}
+
+function keyPressed(){
+  if(key === 'ArrowUp'){
+    personagem.pula();
+    somDoPulo.play();
+  }
 }
 
 function draw() {
@@ -24,6 +89,15 @@ function draw() {
   cenario.move();
 
   personagem.exibe();
+  personagem.aplicaGravidade();
+
+  inimigo.exibe();
+  inimigo.move();
+
+  if(personagem.estaColidindo(inimigo)){
+    console.log('colidiu');
+    noLoop();
+  }
 
   // A ordem de declaração é a mesma em que é "desenhado" e sobreposto
   
