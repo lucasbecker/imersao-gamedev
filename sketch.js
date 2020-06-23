7dab2f1a3302c41251a51ed9a2feb0458bb44064
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-06-23 12:57:15 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,8 +74,8 @@ function setup() {
   inimigo = new Inimigo(matrizInimigo, imagemInimigo, width-52, 52, 52, 104, 104);
   
   frameRate(40);
-  trilha.play().loop(); // Teste para a musica incial automaticamente
-  //trilha.loop();
+  trilha.play(); // Teste para a musica incial automaticamente
+  trilha.loop();
 }
 
 function keyPressed(){
