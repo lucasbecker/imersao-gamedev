9a71be3443e108424ff9344c31b2c9487848848b
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-06-24 10:16:51 -0300

    Update Dia 3

--- README.md
+++ README.md
@@ -41,3 +41,12 @@ Desafios:
 - [ ] Faça um background com aparência 3D usando duas imagens diferentes, com velocidades diferentes. É o efeito paralaxe. Para isso, você precisa que o da frente tenha transparência
 - [ ] Coloque elementos diferentes em seu jogo: abelhas, borboletas, o que você achar que vai ficar legal!
 - [ ] Faça uma tela de game over mais elaborada, explicando o que aconteceu com a personagem e porque a pessoa perdeu.
+
+## Aula 3: Mais inimigos e pontuação
+Incluindo diferentes tipos de desafios, painel de pontos e tela de fim de jogo.
+
+Desafios:
+- [ ] Configure o jogo para que tenhamos apenas um inimigo aparecendo de cada vez.
+- [ ] A colisão da Hipsta com os inimigos não está muito legal...será que conseguimos melhorar isso?
+- [ ] Crie uma tela inicial para o jogo.
+- [ ] Faça com que moedas apareçam durante o jogo e, conforme a Hipsta as pega, ela acumula mais pontos.
--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 let imagemCenario;
 let imagemPersonagem;
 let imagemInimigo;
+let imagemInimigoGrande;
+let imagemInimigoVoador;
+let imagemGameOver;
 
+let pontuacao;
 let cenario;
 let trilha;
 let somDoPulo;
+let gameOver;
+
 
 let personagem;
 let inimigo;
+let inimigoGrande;
+let inimigoVoador;
 
 const matrizInimigo = [
   [0, 0],
@@ -59,10 +67,65 @@ const matrizPersonagem = [
   [660,810],
 ]
 
+const matrizInimigoGrande = [
+  [0,0],
+  [400,0],
+  [800,0],
+  [1200,0],
+  [1600,0],
+  [0,400],
+  [400,400],
+  [800,400],
+  [1200, 400],
+  [1600, 400],
+  [0,800],
+  [400, 800],
+  [800, 800],
+  [1200, 800],
+  [1600, 800],
+  [0, 1200],
+  [400, 1200],
+  [800, 1200],
+  [1200, 1200],
+  [1600, 1200], 
+  [0, 1600],
+  [400, 1600],
+  [800, 1600],
+  [1200, 1600],
+  [1600, 1600],
+  [0, 2000],
+  [400, 2000],
+  [800, 2000],
+]
+
+const matrizInimigoVoador = [
+  [0,0],
+  [200, 0],
+  [400, 0],
+  [0, 150],
+  [200, 150],
+  [400, 150],
+  [0, 300],
+  [200, 300],
+  [400, 300],
+  [0, 450],
+  [200, 450],
+  [400, 450],
+  [0, 600],
+  [200, 600],
+  [400, 600],
+  [0, 750],
+]
+
+const inimigos = [];
+
 function preload(){
   imagemCenario = loadImage("img/cenario/floresta.png");
+  imagemGameOver = loadImage("img/assets/game-over.png");
   imagemPersonagem = loadImage("img/personagem/correndo.png");
   imagemInimigo = loadImage("img/inimigos/gotinha.png");
+  imagemInimigoGrande = loadImage("img/inimigos/troll.png");
+  imagemInimigoVoador = loadImage("img/inimigos/gotinha-voadora.png");
 
   trilha = loadSound("sound/trilha_jogo.mp3");
   somDoPulo = loadSound("sound/somPulo.mp3"); 
@@ -71,10 +134,18 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
+  pontuacao = new Pontuacao();
   cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
-  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width-52, 52, 52, 104, 104);
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270);
+  
+  const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width-52, 30, 52, 52, 104, 104, 10, 200);
+  const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width-52, 200, 100, 75, 200, 150, 10, 1500);
+  const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width-200, 0, 200, 200, 400, 400, 10, 2500);
 
+  inimigos.push(inimigo);
+  inimigos.push(inimigoGrande);
+  inimigos.push(inimigoVoador);
+ 
   frameRate(40);
   trilha.loop();
 }
@@ -90,16 +161,21 @@ function draw() {
   cenario.exibe();
   cenario.move();
 
+  pontuacao.exibe();
+  pontuacao.adicionarPonto();
+
   personagem.exibe();
   personagem.aplicaGravidade();
 
-  inimigo.exibe();
-  inimigo.move();
+  inimigos.forEach(inimigo => {
+    inimigo.exibe();
+    inimigo.move();
 
-  if(personagem.estaColidindo(inimigo)){
-    console.log('colidiu');
-    noLoop();
-  }
+    if(personagem.estaColidindo(inimigo)){
+      image(imagemGameOver, width/2 - 200, height/3);
+      noLoop();
+    }
+  });
 
   // A ordem de declaração é a mesma em que é "desenhado" e sobreposto
   
