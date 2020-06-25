9a52054407f0937c3b14a70bd98bc3ac0c9525e1
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-06-25 11:18:31 -0300

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,14 +1,14 @@
 function preload(){
-    imagemCenario = loadImage("../img/cenario/floresta.png");
-    imagemGameOver = loadImage("../img/assets/game-over.png");
-    imagemPersonagem = loadImage("../img/personagem/correndo.png");
-    imagemInimigo = loadImage("../img/inimigos/gotinha.png");
-    imagemInimigoGrande = loadImage("../img/inimigos/troll.png");
-    imagemInimigoVoador = loadImage("../img/inimigos/gotinha-voadora.png");
+    imagemCenario = loadImage("img/cenario/floresta.png");
+    imagemGameOver = loadImage("img/assets/game-over.png");
+    imagemPersonagem = loadImage("img/personagem/correndo.png");
+    imagemInimigo = loadImage("img/inimigos/gotinha.png");
+    imagemInimigoGrande = loadImage("img/inimigos/troll.png");
+    imagemInimigoVoador = loadImage("img/inimigos/gotinha-voadora.png");
   
-    imagemTelaInicial = loadImage("../img/assets/telainicial.png");
-    fonteTelaInicial = loadFont("../img/assets/fonteTelaInicial.otf");
+    imagemTelaInicial = loadImage("img/assets/telainicial.png");
+    fonteTelaInicial = loadFont("img/assets/fonteTelaInicial.otf");
 
-    trilha = loadSound("../sound/trilha_jogo.mp3");
-    somDoPulo = loadSound("../sound/somPulo.mp3"); 
+    trilha = loadSound("sound/trilha_jogo.mp3");
+    somDoPulo = loadSound("sound/somPulo.mp3"); 
   }
